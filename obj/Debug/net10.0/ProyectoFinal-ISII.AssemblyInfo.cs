05472f776f3d4da961f59487f87ba015a8bafdc8
SofiaaCruz@gmail.com <sofiaacruz187@gmail.com>
2026-03-09 22:32:02 -0300

    build: limpiar repositorio de archivos temporales

--- obj/Debug/net10.0/ProyectoFinal-ISII.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoFinal-ISII.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal-ISII")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d74d1f8ef777b20c0242f1c0d3368bf00b5f263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dde130fcdfa4befe88df3f0b0733b4a167b02fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal-ISII")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal-ISII")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
